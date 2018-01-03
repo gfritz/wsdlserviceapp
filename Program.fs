722d3dc85e2564c20f45b9feb2066a0b9245dd25
= <=>
2018-01-03 17:26:42 -0500

    repro of FS2024 for WsdlService type provider after following steps in visualfsharp/issues/3303

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+// nuget opens
+open System.ServiceModel
+open FSharp.Data.TypeProviders
+
+type Service = WsdlService<" http://www.webservicex.net/country.asmx?WSDL">
+
+try
+    let client = Service.GetcountrySoap()
+    let result = client.GetCurrencies()
+    printfn "GetCurrencies(): %s" result
+with
+| :? ServerTooBusyException as exn ->
+    let innerMessage =
+        match (exn.InnerException) with
+        | null -> ""
+        | innerExn -> innerExn.Message
+    printfn "An exception occurred:\n %s\n %s" exn.Message innerMessage
+| exn -> printfn "An exception occurred: %s" exn.Message
